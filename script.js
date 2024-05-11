ddbc6253be06aedd07df3433a689a36ca0db4c3e
azadeh <azadehazt@gmail.com>
2024-05-11 11:57:07 +0430

    check for hits function added

--- script.js
+++ script.js
@@ -101,4 +101,18 @@ function moveOutcome() {
   }
 }
 
+function checkForHits(squares) {
+  if (
+    (currentSnake[0] + width >= width * width && direction === width) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] - width <= 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains("snake")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
